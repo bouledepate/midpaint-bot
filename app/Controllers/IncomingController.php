31c04a0ee99beb7e35c3d95bb2cf61b4f365b1f0
sshmik <sshmik@wooppay.kz>
2023-04-19 17:54:06 +0600

    midpaint v0.1.3.0

--- app/Controllers/IncomingController.php
+++ app/Controllers/IncomingController.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
+use Longman\TelegramBot\Request;
+use Longman\TelegramBot\Telegram;
 use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
@@ -18,6 +20,18 @@ final class IncomingController
 
     public function __invoke(ServerRequestInterface $request): ResponseInterface
     {
+        $bot = new Telegram($_ENV['TOKEN']);
+        $body = $request->getParsedBody();
+
+        file_put_contents('a.json', json_encode($body));
+
+        $payload = [
+            'chat_id' => $body['message']['chat']['id'],
+            'text' => $body['message']['text']
+        ];
+
+        Request::sendMessage($payload);
+
         return $this->factory->createResponse(204);
     }
 }
